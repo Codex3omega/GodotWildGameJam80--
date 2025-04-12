8546a08c3a73b1d921e2f258919cf9fdb800e1d8
Ktosox <ktoso3@gmail.com>
2025-04-12 17:05:39 +0200

    added more signals

--- Game/soldier_inputs/observer.gd
+++ Game/soldier_inputs/observer.gd
@@ -1,5 +1,14 @@
 extends Area2D
 
+signal enemy_found(how_many:int) # emitted each time an enemy eneters visilbity range
+signal enemy_lost # emitted when last enemy leaves visiblity range
+
+signal thing_found(type_of_thing:int) # emitted each time a thing is seen for the very first time
+
+signal player_found # emitted when player enters visibility range
+signal player_lost # emitted when player leaves visibility range
+
+var array_of_things_already_seen = [] # to avoid a possible loop of re-seeing one thing over and over each seen thing should be added here
 
 func _on_body_entered(body: Node2D) -> void:
 	
